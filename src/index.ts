587629376d5c6f06e04f75ec9d00e64ecaee73e8
abhilash <abhilashpujari11@gmail.com>
2021-01-21 13:14:04 +0530

    added env file

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,12 @@
 import dotenv from 'dotenv';
 import express from 'express';
 
-// Loads environment from .env file
+// Loads environment variables from .env file
 dotenv.config();
 
 const PORT = process.env.PORT || 5000;
 const app = express();
 
 app.listen(PORT, () => {
-   console.log("Server Started 1");
+   console.log("Server Started 1 ", PORT);
 });
