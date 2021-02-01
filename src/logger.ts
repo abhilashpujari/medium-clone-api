6712e96772e7358e232d71b935777d0167ab4281
abhilash <abhilashpujari11@gmail.com>
2021-02-01 11:39:14 +0530

    Updated logger

--- src/logger.ts
+++ src/logger.ts
@@ -1,4 +1,4 @@
-import {createLogger} from "winston";
+import {addColors, createLogger, format} from "winston";
 import DailyRotateFile from "winston-daily-rotate-file";
 
 const transport = new DailyRotateFile({
@@ -10,10 +10,73 @@ const transport = new DailyRotateFile({
     zippedArchive: true
 });
 
-const logger = createLogger({
-    transports: [
-        transport
-    ]
-});
+const customLevels = {
+    levels: {
+        trace: 5,
+        debug: 4,
+        info: 3,
+        warn: 2,
+        error: 1,
+        fatal: 0,
+    },
+    colors: {
+        trace: 'white',
+        debug: 'green',
+        info: 'green',
+        warn: 'yellow',
+        error: 'red',
+        fatal: 'red',
+    },
+};
 
-export default logger;
+const formatter = format.combine(
+    format.colorize(),
+    format.timestamp({format: 'YYYY-MM-DD HH:mm:ss'}),
+    format.splat(),
+    format.printf((info) => {
+        const {timestamp, level, message, ...meta} = info;
+
+        return `${timestamp} [${level}]: ${message} ${
+            Object.keys(meta).length ? JSON.stringify(meta, null, 2) : ''
+        }`;
+    }),
+);
+
+class Logger {
+    private logger;
+
+    constructor() {
+        this.logger = createLogger({
+            level: process.env.NODE_ENV === 'production' ? 'trace' : 'error',
+            levels: customLevels.levels,
+            transports: [transport],
+        });
+        addColors(customLevels.colors);
+    }
+
+    trace(msg: any, meta?: any) {
+        this.logger.log('trace', msg, meta);
+    }
+
+    debug(msg: any, meta?: any) {
+        this.logger.debug(msg, meta);
+    }
+
+    info(msg: any, meta?: any) {
+        this.logger.info(msg, meta);
+    }
+
+    warn(msg: any, meta?: any) {
+        this.logger.warn(msg, meta);
+    }
+
+    error(msg: any, meta?: any) {
+        this.logger.error(msg, meta);
+    }
+
+    fatal(msg: any, meta?: any) {
+        this.logger.log('fatal', msg, meta);
+    }
+}
+
+export default new Logger();
