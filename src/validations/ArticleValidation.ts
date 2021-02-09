cd38faf0a71dfa1379236909a7b5b385e926930a
abhilash <abhilashpujari11@gmail.com>
2021-02-09 19:36:43 +0530

    Added  article update validation

--- src/validations/ArticleValidation.ts
+++ src/validations/ArticleValidation.ts
@@ -1,11 +1,20 @@
 import Joi from 'joi';
 
 const createSchema = Joi.object({
-    email: Joi.string().email(),
-    password: Joi.string().min(3).required(),
-    username: Joi.string().lowercase().alphanum().min(3).max(30).required()
+    title: Joi.string().min(3).max(255).required(),
+    description: Joi.string().min(1).max(255).required(),
+    body: Joi.string().required(),
+    tagList: Joi.array().items(Joi.string())
+});
+
+const updateSchema = Joi.object({
+    title: Joi.string().min(3).max(255),
+    description: Joi.string().min(3).max(255),
+    body: Joi.string().min(1),
+    tagList: Joi.array().items(Joi.string())
 });
 
 export default {
-    createSchema
+    createSchema,
+    updateSchema
 };
