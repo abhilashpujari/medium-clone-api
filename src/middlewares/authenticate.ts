b68774a4d3efd8eb9a4ce18b81241bf7a48011a7
abhilash <abhilashpujari11@gmail.com>
2021-02-09 19:37:47 +0530

    Updated authenticate middleware

--- src/middlewares/authenticate.ts
+++ src/middlewares/authenticate.ts
@@ -1,6 +1,8 @@
 import ApiException from "../exceptions/ApiException";
 import JWTUtils from "../utils/JWTUtils";
 import TokenMistMatchException from "../exceptions/TokenMistMatchException";
+import {getManager} from "typeorm";
+import {User} from "../entity/User";
 
 function authenticate() {
     return async (req, res, next) => {
@@ -18,7 +20,17 @@ function authenticate() {
         }
 
         try {
-            req.user = await JWTUtils.decode(tokenValue);
+            let decodedData  = await JWTUtils.decode(tokenValue);
+            const entityManager = getManager();
+            // @ts-ignore
+            const user = await entityManager.getRepository(User).findOne({id: decodedData.id});
+
+            if (!user) {
+                next(new ApiException('User not found', 404));
+                return;
+            }
+
+            req.user = user;
             next();
         } catch (error) {
             next(new TokenMistMatchException('Token mismatch'));
